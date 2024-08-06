249eab05d866857541917f927d97867a0a75966f
Oganes <og.marckaryan@yandex.ru>
2024-08-06 18:46:13 +0300

    config ts

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import './style/style.css'
+const inputHeight = document.createElement('input');
+inputHeight.type = 'number';
+inputHeight.min = '0';
+inputHeight.placeholder = 'Столбец';
+const inputWidth = document.createElement('input');
+inputWidth.type = 'number';
+inputWidth.min = '0';
+inputWidth.placeholder = 'Строка';
+
+const body: HTMLElement | null = document.querySelector('body');
+console.log(body);
+body?.prepend(document.createElement('menu'));
+
+const menu: HTMLElement | null  = document.querySelector('menu');
+menu?.append(inputHeight,inputWidth);
+
+function createField() {
+    // for (let i = 0; i < Number(inputHeight.value); i+=1){
+    //     let tr = document.createElement('tr');
+        
+    //     for (let j = 0; j < Number(inputWidth.value); j+=1){
+    //         let td = document.createElement('td');
+    //         tr.appendChild(td)
+    //     }
+    //     document.querySelector('table')?.append(tr);
+    // }
+}
+inputHeight.addEventListener('change',createField)
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle-prod.js",
+    path: path.resolve(__dirname, "prod"),
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    "@babel/preset-env",
+                    "@babel/preset-typescript",
+                ]
+            }
+        }
+    },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    port: 8081,
+  },
+  plugins: [
+    // спросить у преподавтеля как описать цикл, чтобы пройтись по всем html файлам в папке
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
