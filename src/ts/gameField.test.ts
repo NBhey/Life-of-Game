b83f832525481578b031c8f62e98559579c41500
Oganes <og.marckaryan@yandex.ru>
2024-08-07 20:06:49 +0300

    make getState

--- src/ts/gameField.test.ts
+++ src/ts/gameField.test.ts
@@ -1,9 +1,51 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test, beforeEach } from "@jest/globals";
 import GameField from "./gameField";
 
-describe('Game Field', ()=>{
-    test("is a class", () => {
-      expect(GameField).toBeInstanceOf(Function);
-      expect(new GameField()).toBeInstanceOf(GameField);
-    })
-})
+describe("Game Field", () => {
+  test("is a class", () => {
+    expect(GameField).toBeInstanceOf(Function);
+    expect(new GameField()).toBeInstanceOf(GameField);
+  });
+
+  test("has a function getState", () => {
+    const gameField = new GameField();
+    expect(gameField.getState).toBeInstanceOf(Function);
+    expect(gameField.getState()).toEqual([[]]);
+  });
+
+  describe("functional tests", () => {
+    const width = 2;
+    const height = 3;
+    let gameField;
+    beforeEach(() => {
+      gameField = new GameField(height, width);
+    });
+
+    test("supports settings side from constructor", () => {
+      expect(gameField.getState()).toEqual([
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ]);
+    });
+
+    test("has .toggleCellState method", () => {
+      expect(gameField.toggleCellState).toBeInstanceOf(Function);
+      const [x1, y1] = [0, 0];
+      const [x2, y2] = [1, 2];
+      gameField.toggleCellState(x1, y1);
+      gameField.toggleCellState(x2, y2);
+      expect(gameField.getState()).toEqual([
+        [1, 0],
+        [0, 0],
+        [0, 1],
+      ]);
+      gameField.toggleCellState(x2, y2);
+      expect(gameField.getState()).toEqual([
+        [1, 0],
+        [0, 0],
+        [0, 0],
+      ]);
+    });
+  });
+});
--- src/ts/gameField.ts
+++ src/ts/gameField.ts
@@ -1,10 +1,28 @@
-type Cell = number;
 interface IGameField {
-    getState():Cell[][];
+    getState():number[][];
+    toggleCellState(x:number, y:number):[];
+
 }
 
 
-// export default class GameField implements IGameField {
-   
+export default class GameField implements IGameField {
+    height: number;
+    width: number;
+    constructor(height:number = 1, width:number = 0){
+       this.height = height;
+       this.width = width;
+    }
+    getState():number[][]{
+        const matrix = []
+        for(let i = 0; i < this.height; i+=1){
+            matrix.push([])
+            for(let j = 0; j < this.width; j +=1){
+                matrix[i].push(0)
+            }
+        }
+        return matrix
+    }
+
+    toggleCellState(x:number, y:number){}
     
-// }
+}
