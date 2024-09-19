b8a6dd1e99bf97179eb1644190fef16669efac22
Oganes <og.marckaryan@yandex.ru>
2024-09-19 11:43:22 +0300

    life of game

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
-import './style/style.css'
-const inputHeight = document.createElement('input');
-inputHeight.type = 'number';
-inputHeight.min = '0';
-inputHeight.placeholder = 'Столбец';
-const inputWidth = document.createElement('input');
-inputWidth.type = 'number';
-inputWidth.min = '0';
-inputWidth.placeholder = 'Строка';
+import { Game } from "./ts/Game";
+import GameField from "./ts/gameField";
+import GameView  from "./ts/GameView";
+import "./style/style.css";
 
-const body: HTMLElement | null = document.querySelector('body');
-console.log(body);
-body?.prepend(document.createElement('menu'));
+const el = document.getElementById("app") as HTMLElement;
 
-const menu: HTMLElement | null  = document.querySelector('menu');
-menu?.append(inputHeight,inputWidth);
-
-function createField() {
-   const tr:HTMLElement =  document.createElement('tr');
-   const td:HTMLElement =  document.createElement('td');
-   tr.append(td);
-   document.querySelector('table')?.append(tr);
-    // for (let i = 0; i < Number(inputHeight.value); i+=1){
-    //     let tr = document.createElement('tr');
-        
-    //     for (let j = 0; j < Number(inputWidth.value); j+=1){
-    //         let td = document.createElement('td');
-    //         tr.appendChild(td)
-    //     }
-    //     document.querySelector('table')?.append(tr);
-    // }
-}
-inputHeight.addEventListener('change',createField)
+const gameView = new GameView(el);
+const gameField = new GameField(10, 10);
+new Game(gameField, gameView, 1000);
